adbb17fcdbb778f40040bde01270b9643c9b01d3
Michael Toth <mtoth003@gmail.com>
2022-05-18 17:32:34 -0400

    added collisions and finished game

--- index.js
+++ index.js
@@ -5,21 +5,69 @@ import BulletController from "./BulletController.js";
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
+const restartButton = document.querySelector(".restart-btn");
+restartButton.addEventListener("click", () => {
+  location.reload();
+});
+
 canvas.width = 600;
 canvas.height = 600;
 
 const background = new Image();
 background.src = "images/space.png";
 
-const enemyController = new EnemyController(canvas);
 const playerBulletController = new BulletController(canvas, 10, "red", true);
+const enemyBulletController = new BulletController(canvas, 4, "white", false);
+const enemyController = new EnemyController(
+  canvas,
+  enemyBulletController,
+  playerBulletController
+);
 const player = new Player(canvas, 3, playerBulletController);
 
+let isGameOver = false;
+let didWin = false;
+
 const game = () => {
+  checkGameOver();
   ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-  // enemyController.draw(ctx);
-  player.draw(ctx);
-  playerBulletController.draw(ctx);
+  displayGameOver();
+  if (!isGameOver) {
+    enemyController.draw(ctx);
+    player.draw(ctx);
+    playerBulletController.draw(ctx);
+    enemyBulletController.draw(ctx);
+    console.log(isGameOver);
+  }
+};
+
+const displayGameOver = () => {
+  if (isGameOver) {
+    let text = didWin ? "You Win!" : "Game Over";
+    let textOffset = didWin ? 3.5 : 5;
+
+    ctx.fillStyle = "white";
+    ctx.font = "70px Arial";
+    ctx.fillText(text, canvas.width / textOffset, canvas.height / 2);
+  }
+};
+
+const checkGameOver = () => {
+  if (isGameOver) {
+    return;
+  }
+  if (enemyBulletController.collideWith(player)) {
+    isGameOver = true;
+  }
+
+  if (enemyController.collideWith(player)) {
+    isGameOver = true;
+  }
+
+  if (enemyController.enemyRows.length === 0) {
+    didWin = true;
+    isGameOver = true;
+  }
 };
 
 setInterval(game, 1000 / 60);
