12cc0e39b239a33f6ac9e2062a325c5ecf27abe4
Michael Toth <mtoth003@gmail.com>
2022-05-18 14:09:19 -0400

    finished enemy movement

--- EnemyController.js
+++ EnemyController.js
@@ -1,4 +1,5 @@
 import Enemy from "./Enemy.js";
+import MovingDirection from "./MovingDirection.js";
 
 export default class EnemyController {
   enemyMap = [
@@ -12,17 +13,84 @@ export default class EnemyController {
 
   enemyRows = [];
 
+  currentDirection = MovingDirection.right;
+  xVelocity = 0;
+  yVelocity = 0;
+  defaultXVelocity = 1;
+  defaultYVelocity = 1;
+  moveDownTimerDefault = 30;
+  moveDownTimer = this.moveDownTimerDefault;
+
   constructor(canvas) {
     this.canvas = canvas;
     this.createEnemies();
   }
 
   draw(ctx) {
+    this.decrementMoveDownTimer();
+    this.updateVelocityAndDirection();
     this.drawEnemies(ctx);
+    this.resetMoveDownTimer();
+  }
+
+  resetMoveDownTimer() {
+    if (this.moveDownTimer <= 0) {
+      this.moveDownTimer = this.moveDownTimerDefault;
+    }
+  }
+
+  decrementMoveDownTimer() {
+    if (
+      this.currentDirection === MovingDirection.downLeft ||
+      this.currentDirection === MovingDirection.downRight
+    ) {
+      this.moveDownTimer--;
+    }
+  }
+
+  updateVelocityAndDirection() {
+    for (const enemyRow of this.enemyRows) {
+      if (this.currentDirection === MovingDirection.right) {
+        this.xVelocity = this.defaultXVelocity;
+        this.yVelocity = 0;
+        const rightMostEnemy = enemyRow[enemyRow.length - 1];
+        if (rightMostEnemy.x + rightMostEnemy.width >= this.canvas.width) {
+          this.currentDirection = MovingDirection.downLeft;
+          break;
+        }
+      } else if (this.currentDirection === MovingDirection.downLeft) {
+        if (this.moveDown(MovingDirection.left)) {
+          break;
+        }
+      } else if (this.currentDirection === MovingDirection.left) {
+        this.xVelocity = -this.defaultXVelocity;
+        this.yVelocity = 0;
+        const leftMostEnemy = enemyRow[0];
+        if (leftMostEnemy.x <= 0) {
+          this.currentDirection = MovingDirection.downRight;
+          break;
+        }
+      } else if (this.currentDirection === MovingDirection.downRight) {
+        if (this.moveDown(MovingDirection.right)) {
+          break;
+        }
+      }
+    }
+  }
+
+  moveDown(newDirection) {
+    this.xVelocity = 0;
+    this.yVelocity = this.defaultYVelocity;
+    if (this.moveDownTimer <= 0) {
+      this.currentDirection = newDirection;
+      return true;
+    }
+    return false;
   }
 
   drawEnemies(ctx) {
     this.enemyRows.flat().forEach((enemy) => {
+      enemy.move(this.xVelocity, this.yVelocity);
       enemy.draw(ctx);
     });
   }
